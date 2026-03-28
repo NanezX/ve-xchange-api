83038b11c51fc5b9248c586385a90292bfb99fbf
NanezX <victor.nanezj@gmail.com>
2026-03-28 13:46:35 -0400

    Refactor main function to load configuration and use AppConfig for server port

--- main.go
+++ main.go
@@ -8,12 +8,18 @@ import (
 // var appState AppState = AppState{}
 
 func main() {
-	port := 8080
+	err := LoadConfig()
+
+	if err != nil {
+		fmt.Printf("Failed to load env file... [Error]: %v", err)
+		return
+	}
+
 	mux := http.NewServeMux()
 
 	mux.Handle("/hello", HelloWorldHandler{})
 	mux.Handle("/rates", RatesHandler{})
 
-	fmt.Printf("Servidor corriendo en http://localhost:%d\n", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+	fmt.Printf("Servidor corriendo en http://localhost:%d\n", AppConfig.AppPort)
+	http.ListenAndServe(fmt.Sprintf(":%d", AppConfig.AppPort), mux)
 }
