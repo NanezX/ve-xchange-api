807cafe142bcc0299e3967a2de000278bc5b69a7
NanezX <victor.nanezj@gmail.com>
2026-03-28 22:02:08 -0400

    Refactor RatesHandler to use AppState for exchange rates and remove unused price fetching logic

--- handlers.go
+++ handlers.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 type HelloWorldHandler struct{}
@@ -16,33 +15,10 @@ func (HelloWorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 type RatesHandler struct{}
 
 func (RatesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// TODO: Read from stored or something - The values should be updated independently
-	dolarVzlaProvider := NewDolarVzlaProvider()
-
-	vlzaData, err := dolarVzlaProvider.GetPrices()
-	if err != nil {
-		http.Error(w, "Error obteniendo datos del BCV", http.StatusInternalServerError)
-		return
-	}
-
-	binaProvider := NewBinanceProvider()
-
-	bnbPrices, err := binaProvider.GetPrices()
-	if err != nil {
-		fmt.Printf("error bnb: %v", err)
-		http.Error(w, "Error obteniendo datos del Binance p2p", http.StatusInternalServerError)
-		return
-	}
-
-	//
-	data := ExchageRates{
-		UsdBCV:      vlzaData["USD_BCV"],
-		EurBCV:      vlzaData["EUR_BCV"],
-		UsdtBinance: bnbPrices["USDT_BINANCE"],
-		LastUpdate:  time.Now(),
-	}
+	AppState.RLock()
+	defer AppState.RUnlock()
 
 	w.Header().Set("Content-Type", "application/json")
 
-	json.NewEncoder(w).Encode(data)
+	json.NewEncoder(w).Encode(AppState.Rates)
 }
