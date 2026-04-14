4cb97644f2243279ec9309720463662147a3373a
NanezX <victor.nanezj@gmail.com>
2026-04-14 12:50:33 -0400

    Add fetchJson function for improved JSON fetching with error handling

---
+++ utils.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func fetchJson[T any](client *http.Client, req *http.Request) (T, error) {
+	var result T
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return result, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		errorBody, _ := io.ReadAll(resp.Body)
+		return result, fmt.Errorf("Failed to fetch json. Status code: %d - Error: %s", resp.StatusCode, string(errorBody))
+	}
+
+	// Write the response
+	err = json.NewDecoder(resp.Body).Decode(&result)
+
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
