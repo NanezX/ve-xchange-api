36fa306f074957a69c1ba8bae43548c3be42e83e
NanezX <victor.nanezj@gmail.com>
2026-04-14 11:12:57 -0400

    dedicated functions to update prices

--- state.go
+++ state.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sync"
+	"time"
 )
 
 type State struct {
@@ -21,4 +22,21 @@ func (s *State) GetRates() ExchangeRates {
 	s.RLock()
 	defer s.RUnlock()
 	return s.Rates
+}
+
+func UpdateBcvPrice(data PriceResponse) {
+	AppState.Lock()
+	defer AppState.Unlock()
+
+	AppState.Rates.UsdBCV = data["USD_BCV"]
+	AppState.Rates.EurBCV = data["EUR_BCV"]
+	AppState.Rates.LastUpdate = time.Now()
+}
+
+func UpdateBinancePrice(data PriceResponse) {
+	AppState.Lock()
+	defer AppState.Unlock()
+
+	AppState.Rates.UsdtBinance = data["USDT_BINANCE"]
+	AppState.Rates.LastUpdate = time.Now()
 }
