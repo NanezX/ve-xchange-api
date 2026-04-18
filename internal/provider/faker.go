bd90673eae58bcf52fc91f25b66d043df42c0f42
NanezX <victor.nanezj@gmail.com>
2026-04-17 21:43:00 -0400

    Refactor FakeHTTPDoer Do method to improve error handling and streamline response generation

--- internal/provider/faker.go
+++ internal/provider/faker.go
@@ -10,17 +10,22 @@ type FakeHTTPDoer struct {
 	StatusCode int
 	Body       string
 	Error      error
+	DoFunc     func(*http.Request) (string, error)
 }
 
-func (f *FakeHTTPDoer) Do(*http.Request) (*http.Response, error) {
-	if f.Error != nil {
-		return nil, f.Error
+func (f *FakeHTTPDoer) Do(req *http.Request) (*http.Response, error) {
+	if f.DoFunc == nil {
+		return &http.Response{StatusCode: f.StatusCode, Body: io.NopCloser(strings.NewReader(f.Body))}, nil
 	}
 
-	resp := &http.Response{
+	// Do func is to inject something specific
+	body, err := f.DoFunc(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &http.Response{
 		StatusCode: f.StatusCode,
-		Body:       io.NopCloser(strings.NewReader(f.Body)),
-	}
-
-	return resp, nil
+		Body:       io.NopCloser(strings.NewReader(body)),
+	}, nil
 }
