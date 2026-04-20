177a018f38e31983e1e47bbc55e316af32d77a33
NanezX <victor.nanezj@gmail.com>
2026-04-20 13:42:25 -0400

    Add error handling tests for Worker to validate provider error scenarios

--- internal/worker/worker_test.go
+++ internal/worker/worker_test.go
@@ -1,6 +1,8 @@
 package worker
 
 import (
+	"errors"
+	"sync"
 	"testing"
 	"time"
 
@@ -8,12 +10,19 @@ import (
 )
 
 type MockProvider struct {
-	prices   rates.PriceResponse
-	isCalled bool
+	mu         sync.Mutex
+	prices     rates.PriceResponse
+	priceError error
+	isCalled   bool
 }
 
 func (p *MockProvider) GetPrices() (rates.PriceResponse, error) {
+	p.mu.Lock()
 	p.isCalled = true
+	p.mu.Unlock()
+	if p.priceError != nil {
+		return nil, p.priceError
+	}
 	return p.prices, nil
 }
 
@@ -22,7 +31,6 @@ func (p *MockProvider) GetName() string {
 }
 
 func TestWorkerApplyData(t *testing.T) {
-	//
 	mockProvider := &MockProvider{
 		prices: rates.PriceResponse{"USD": 543.21},
 	}
@@ -49,4 +57,32 @@ func TestWorkerApplyData(t *testing.T) {
 		t.Fatalf("Expected Apply function to be called")
 	}
 
+}
+
+func TestWorkerProviderError(t *testing.T) {
+	mockProvider := &MockProvider{
+		priceError: errors.New("error to get prices"),
+	}
+
+	applyCalled := false
+
+	job := ProviderJob{
+		Provider: mockProvider,
+		Every:    1 * time.Millisecond,
+		Apply: func(rates.PriceResponse) {
+			applyCalled = true
+		},
+	}
+
+	StartPriceWorker([]ProviderJob{job})
+
+	time.Sleep(10 * time.Millisecond)
+
+	if !mockProvider.isCalled {
+		t.Fatalf("Expected Provider GetPrices to be called")
+	}
+
+	if applyCalled {
+		t.Fatalf("Apply functon should not be called on get price error")
+	}
 }
